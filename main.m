87cad21fb07f3c73b44c208d2bc1a09e2f200dbf
Alexander Lamond <alexander.lamond@nottingham.ac.uk>
2019-05-29 11:51:20 +0100

    Added model description

--- main.m
+++ main.m
@@ -2,4 +2,16 @@
 
 % Author: Alexander (Sandy) Lamond
 
-% This script is solves coupled pdes for dissolution of reaction products into surrounding fluid from the surface of a food bolus.
+% This script models the dissolution of food bolus into the surrounding
+% gastric fluid. 
+% Some assumptions are made;
+%     * the bolus is a homogenous sphere.
+%     * the bolus contains starch that is reacted with salivary amylase to
+%     produce glucose, which then diffuses from the surface of the bolus
+%     into the surrounding fluid.
+%     * the surrounding fluid is stagnant and has a finite boundary
+%     * there is no flux at the boundary of fluid
+%     * at the boundary of the bolus and fluid, the flux of glucose
+%     internally is equal to the flux of glucose exteranlly.
+%     * initial condition for the bolus and surrounding fluid is zero
+%     glucose.
