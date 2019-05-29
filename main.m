eab1de970a0c8b2335d2d72e530d599d34a7c910
Alexander Lamond <alexander.lamond@nottingham.ac.uk>
2019-05-29 11:35:15 +0100

    Added code preamble

--- main.m
+++ main.m
@@ -1,1 +1,5 @@
 % Model of dissolution of bolus into surrounding fluid
+
+% Author: Alexander (Sandy) Lamond
+
+% This script is solves coupled pdes for dissolution of reaction products into surrounding fluid from the surface of a food bolus.
